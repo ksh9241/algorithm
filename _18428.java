b64d2794f82b9658eed00a51afa7b12ec9f48d5c
ksh9241 <ksh9241@naver.com>
2022-01-20 21:57:07 +0900

    소스 3번 바꿔서 풀었네..

--- _18428.java
+++ _18428.java
@@ -4,72 +4,88 @@ import java.util.StringTokenizer;
 public class Main {
 	static String [][] CLASS;
 	static int N, count;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
 	
 	public static void main (String [] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
 		
-		CLASS = new String[N + 2][N + 2];
+		CLASS = new String[N][N];
 		
-		for (int i = 1; i <= N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
+			for (int j = 0; j < N; j++) {
 				CLASS[i][j] = st.nextToken();
 			}
 		}
 		
-		String result = "";
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if ("T".equals(CLASS[i][j])) {
-					for (int k = 0; k < 4; k++) {
-						if ("S".equals(CLASS[i + dx[k]][j + dy[k]])) count = 999;
-						boolean b = backTraking(i,j, k);
-						if (b && "X".equals(CLASS[i + dx[k]][j + dy[k]])) {
-							CLASS[i + dx[k]][j + dy[k]] = "O";
-							count++;
+		backTraking(0,0,0);
+		
+		System.out.println("NO");
+	}
+	static void backTraking (int x, int y, int depth) {
+		if (depth == 3) {
+			boolean chk = true;
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if ("T".equals(CLASS[i][j])) {
+						if (!check(i,j)) {
+							chk = false;
 						}
 					}
-					info();
+				}
+			}
+			
+			if (chk) {
+				System.out.println("YES");
+				System.exit(0);
+			}
+			
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if ("X".equals(CLASS[i][j])) {
+					CLASS[i][j] = "O";
+					backTraking(i, j, depth + 1);
+					CLASS[i][j] = "X";
 				}
 			}
 		}
-		
-		if (count > 3) {
-			result = "NO";
-		} else {
-			result = "YES";
-		}
-		
-		System.out.println(result);
-	}
-
-	
-	static boolean backTraking(int x, int y, int idx) {
-		if (x > N || y > N || x <= 0 || y <= 0) {
-			return false;
-		}
-		
-		if ("S".equals(CLASS[x][y])) {
-			return true;
-		}
-		else if ("O".equals(CLASS[x][y])) {
-			return false;
-		}
-		else {
-			return backTraking(x + dx[idx], y + dy[idx], idx);
-		}
 	}
 	
-	static void info () {
-		for (String[] i : CLASS) {
-			for (String j : i) {
-				System.out.print(j + " ");
-			}
-			System.out.println();
+	static boolean check (int x, int y) {
+		boolean chk = true;
+		for (int i = x; i < N; i++ ) {
+			if ("S".equals(CLASS[i][y])) {
+				chk = false;
+				break;
+			} else if ("O".equals(CLASS[i][y])) break;
 		}
+		
+		for (int i = x; i >= 0; i--) {
+			if ("S".equals(CLASS[i][y])) {
+				chk = false;
+				break;
+			} else if ("O".equals(CLASS[i][y])) break;
+		}
+		
+		for (int i = y; i < N; i++) {
+			if ("S".equals(CLASS[x][i])) {
+				chk = false;
+				break;
+			} else if ("O".equals(CLASS[x][i])) break;
+		}
+		
+		for (int i = y; i >= 0; i--) {
+			if ("S".equals(CLASS[x][i])) {
+				chk = false;
+				break;
+			} else if ("O".equals(CLASS[x][i])) break;
+		}
+		
+		return chk;
 	}
 }

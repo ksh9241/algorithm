c764f5da591ef47b4318de20cadc2890c286c2f7
KimSeHwan <ksh9241@naver.com>
2022-08-14 19:56:50 +0900

    순열 구현문제?

--- _15686.java
+++ _15686.java
@@ -2,86 +2,79 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int N, M;
 	static int[][] map;
-	static int n, m;
-	
-	static int minDistince = Integer.MAX_VALUE;
-	
-	static List<chickenPoint> homeArr = new ArrayList<>();
-	static List<chickenPoint> chickenArr = new ArrayList<>();
-	static Stack<chickenPoint> selectChicken = new Stack<>();
-	
-	public static void main (String [] args) throws Exception{
+	static boolean[][] visit;
+
+	static int answer = Integer.MAX_VALUE;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		map = new int[n + 1][n + 1];
-		
-		for (int i = 1; i <= n; i++) {
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		map = new int[N + 1][N + 1];
+		visit = new boolean[N + 1][N + 1];
+
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= n; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				map[i][j] = num;
-				
-				if (num == 1) {
-					homeArr.add(new chickenPoint(i, j));
-				} else if (map[i][j] == 2) {
-					chickenArr.add(new chickenPoint(i, j));
+			for (int j = 1; j <= N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		combination(1, 0, new ArrayList<int[]>());
+		System.out.println(answer);
+
+	}
+
+	static void combination(int x, int depth, List<int[]> chichen) {
+		// 치킨집 개수가 맞으면 탐색
+		if(depth == M) {
+			quest(chichen);
+			return;
+		}
+
+		for (int i = x; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				// 순열로만 하는게 아닌 이유는 치킨집만 담아두고 처리하기 위함
+				if (!visit[i][j] && map[i][j] == 2) {
+					int[] a = {i,j}; // idx 0 == i, idx 1 == j
+					chichen.add(a);
+					visit[i][j] = true;
+					combination(i, depth + 1, chichen);
+					chichen.remove(a);
+					visit[i][j] = false;
 				}
 			}
 		}
-		
-		
-		select(0, 0);
-		System.out.println(minDistince);
-	}
-	
-	static void select (int start, int depth) {
-		if (depth == m) {
-			backTraking();
-			return;
-		}
-		
-		for (int i = start; i < chickenArr.size(); i++) {
-			selectChicken.push(chickenArr.get(i));
-			select (start + 1, depth + 1);
-			selectChicken.pop();
-		}
 	}
 
-	static void backTraking() {
-		int sum = 0;
-		for (chickenPoint home : homeArr) {
-			int min = Integer.MAX_VALUE;
-			for (chickenPoint chichens : selectChicken) {
-				int distince = resultAbs (home, chichens);
-				min = Math.min(min, distince);
+	static void quest(List<int[]> chichen) {
+		int totalDistance = 0;
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (map[i][j] == 1) {
+					totalDistance += minDistance(chichen, i, j);
+				}
 			}
-			sum += min;
-			
-			if (sum > minDistince) return;
 		}
-		minDistince = Math.min(minDistince, sum);
+
+		answer = Math.min(answer, totalDistance);
 	}
-	
-	static int resultAbs (chickenPoint home, chickenPoint chichens) {
-		return Math.abs(home.r - chichens.r) +  Math.abs(home.c - chichens.c);
-	}
-	
-	static class chickenPoint {
-		int r;
-		int c;
-		
-		public chickenPoint (int r, int c) {
-			this.r = r;
-			this.c = c;
+
+	static int minDistance(List<int[]> chichen, int x, int y) {
+		int minVal = Integer.MAX_VALUE;
+		int calcNum = 0;
+		for (int i = 0; i < chichen.size(); i++) {
+			int[] val = chichen.get(i);
+			calcNum = Math.abs(val[0] - x) + Math.abs(val[1] - y);
+			minVal = Math.min(minVal, calcNum);
 		}
+		return minVal;
 	}
 }

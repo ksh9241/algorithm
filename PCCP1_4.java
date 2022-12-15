a6ac1e4869153cc63ad200128e82788c28b30624
ksh9241 <ksh9241@naver.com>
2022-12-15 22:46:47 +0900

    소스분석

--- PCCP1_4.java
+++ PCCP1_4.java
@@ -55,3 +55,55 @@ class Solution {
         return q;
     }
 }
+
+// 내가 한거말고 답지 보고 수정한
+import java.util.*;
+class Solution {
+    public long[] solution(int[][] program) {
+        long[] answer = new long[11];
+
+        Arrays.sort(program, (o1, o2) -> {
+            return o1[1] - o2[1];
+        });
+        
+        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
+
+        int count = 0;
+        int lastComp = 0;
+        int idx = 0;
+        int time = 0;
+        while (count < program.length) {
+            
+            for (int i = idx; i < program.length; i++) {
+                
+                if (program[i][1] <= time) {
+                    q.add(program[i]);
+                    idx++;
+                } else {
+                    break;
+                }
+            }
+            
+            if (!q.isEmpty() && lastComp <= time) {
+                int[] pollProgram = q.poll();
+                int number = pollProgram[0];
+                int inTime = pollProgram[1];
+                int endTime = pollProgram[2];
+                
+                lastComp = time + endTime;
+                answer[number] += time - inTime;
+                count++;
+            }
+            
+            time++;
+        }
+        answer[0] = lastComp;
+
+        return answer;
+    }
+}

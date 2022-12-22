8e6f1dfb1616961427c834e8c4d6ff99de097734
ksh9241 <ksh9241@naver.com>
2022-12-22 23:13:19 +0900

    못풀어서 답지봄

---
+++ 야근지수.java
@@ -0,0 +1,41 @@
+class Solution {
+    public long solution(int n, int[] works) {
+        int[] result = new int[works.length];
+        int totalovertime = sumArr(works) - n;
+        int time = totalovertime;
+        
+        if (time <= 0) return 0;
+
+        for (int i=0; i<works.length; i++) {
+            result[i] = (works[i] < totalovertime/works.length)? works[i] : totalovertime/works.length;
+            time -= result[i];
+        }
+        
+        for (int i=0; i<works.length; i++) {
+            if (time <= 0) break;
+            if (works[i] > result[i]) {
+                result[i]++;
+                time--;
+            }
+            if (i == works.length - 1) i = -1;
+        }
+       
+        return getTiredness(result);
+    }
+    
+    long getTiredness(int[] works) {
+        long sum = 0;
+        for (int work : works) {
+            sum += work * work;
+        }
+        return sum;
+    }
+    
+    int sumArr(int[] works) {
+        int sum = 0;
+        for (int work : works) {
+            sum += work;
+        }
+        return sum;
+    }
+}

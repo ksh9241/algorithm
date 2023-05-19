37b062daf554b90997cc38a9de0be233c4930d10
KimSeHwan <ksh9241@naver.com>
2023-05-19 22:51:38 +0900

    답지봄..

--- 순위 검색.java
+++ 순위 검색.java
@@ -1,129 +1,66 @@
 import java.util.*;
 class Solution {
-    Map<String, int[]> map = new HashMap<>();
-    List<Applicant> list = new ArrayList<>();
-    int[] answer;
+	static Map<String, List<Integer>> map = new HashMap<>();
+
     public int[] solution(String[] info, String[] query) {
-        answer = new int[query.length];
-        list = info(info);
-        settingMap(list);
+        int[] answer = new int[query.length];
+
+        for (int i = 0; i < info.length; i++) {
+            dfs("", info[i].split(" "), 0);
+        }
+
+        for (String key : map.keySet()) {
+            Collections.sort(map.get(key));
+        }
 
         for (int i = 0; i < query.length; i++) {
-            answer[i] = check(query[i]);
+            query[i] = query[i].replaceAll(" and ", "");
+            String[] q = query[i].split(" ");
+            int score = Integer.parseInt(q[1]);
+            answer[i] = binarySearch(q[0], score);
         }
 
 
         return answer;
     }
 
-    public int check(String query) {
-        String[] split = query.split(" ");
-        String language = split[0];
-        String type = split[2];
-        String career = split[4];
-        String taste = split[6];
-        String score = split[7];
 
-        int start = 0;
-        int end = list.size() - 1;
+    public int binarySearch(String key, int score) {
+        if (map.containsKey(key)) {
+            List<Integer> list = map.get(key);
+            int start = 0;
+            int end = list.size() - 1;
 
-        if (!"-".equals(language)) {
-            int[] idx = map.get(language);
-            start = idx[0];
-            end = idx[1];
-        }
-
-        int count = 0;
-        for (int i = start; i <= end; i++) {
-            if (("-".equals(type) || list.get(i).type.equals(type))
-                    && ("-".equals(career) || list.get(i).career.equals(career))
-                    && ("-".equals(taste) || list.get(i).taste.equals(taste))
-                    && ("-".equals(score) || list.get(i).score >= Integer.parseInt(score))
-            ) {
-                count++;
+            if (list.get(end) < score) {
+                return 0;
             }
-        }
-        return count;
-    }
 
-    private void settingMap(List<Applicant> list) {
-        String language = "";
-        int start = 0;
-        for (int i = 0; i < list.size(); i++) {
-            Applicant applicant = list.get(i);
-            if (!language.equals(applicant.language)) {
-                if (language.equals("")) {
-                    start = i;
-                    language = applicant.language;
+            while (start < end) {
+                int mid = (start + end) / 2;
+                if (list.get(mid) < score) {
+                    start = mid + 1;
                 } else {
-                    map.put(language, new int[]{start, i - 1});
-                    start = i;
-                    language = applicant.language;
+                    end = mid;
                 }
             }
+            return list.size() - start;
         }
-        map.put(language, new int[]{start, list.size() - 1});
+        return 0;
     }
 
-    public List<Applicant> info(String[] info) {
-        List<Applicant> list = new ArrayList<>();
-
-        for (int i = 0; i < info.length; i++) {
-            StringTokenizer st = new StringTokenizer(info[i]);
-            list.add(new Applicant(st.nextToken(), st.nextToken(), st.nextToken(), st.nextToken(), Integer.parseInt(st.nextToken())));
-        }
-
-        Collections.sort(list);
-
-        return list;
-    }
-}
-
-class Applicant implements Comparable<Applicant> {
-    String language;
-    String type;
-    String career;
-    String taste;
-    int score;
-
-    public Applicant(String language,String type,String career,String taste,int score) {
-        this.language = language;
-        this.type = type;
-        this.career = career;
-        this.taste = taste;
-        this.score = score;
-    }
-
-    @Override
-    public String toString() {
-        return "Applicant{" +
-                "language='" + language + '\'' +
-                ", type='" + type + '\'' +
-                ", career='" + career + '\'' +
-                ", taste='" + taste + '\'' +
-                ", score=" + score +
-                '}';
-    }
-
-    // 점수만 내림차순
-    @Override
-    public int compareTo(Applicant o) {
-        if (language.compareTo(o.language) == 0) {
-            if (type.compareTo(o.type) == 0) {
-                if (career.compareTo(o.career) == 0) {
-                    if (taste.compareTo(o.taste) == 0) {
-                        return o.score - score;
-                    } else {
-                        return taste.compareTo(o.taste);
-                    }
-                } else {
-                    return career.compareTo(o.career);
-                }
-            } else {
-                return type.compareTo(o.type);
+    public void dfs(String s, String[] info, int depth) {
+        if (depth == 4) {
+            if (map.containsKey(s) == false) {
+                List<Integer> list = new ArrayList<>();
+                list.add(Integer.parseInt(info[4]));
+                map.put(s, list);
             }
-        } else {
-            return language.compareTo(o.language);
+            else {
+                map.get(s).add(Integer.parseInt(info[4]));
+            }
+            return;
         }
+        dfs(s + "-", info, depth + 1);
+        dfs(s + info[depth], info, depth + 1);
     }
 }

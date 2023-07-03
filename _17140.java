cf817e91cfbed7c09bf2059c9580efcdef5b23f6
KimSeHwan <ksh9241@naver.com>
2023-07-03 22:10:46 +0900

    오랜만에 풀기

--- _17140.java
+++ _17140.java
@@ -22,6 +22,10 @@ public class Main {
 
         // A[r - 1][c - 1] = k
         int time = 0;
+        if (r - 1 < A.length && c - 1 < A[0].length && A[r - 1][c - 1] == k) {
+            System.out.println(time);
+            return;
+        }
         while (time <= 100) {
             int rCnt = A.length;
             int cCnt = A[0].length;
@@ -29,10 +33,14 @@ public class Main {
             String type = cCnt - rCnt > 0 ? "C" : "R";
             check(type);
 
-            if (A[r - 1][c - 1] == k) {
+            if (r - 1 < A.length && c - 1 < A[0].length && A[r - 1][c - 1] == k) {
+                time++;
                 break;
             }
             time++;
+        }
+        if (time > 100) {
+            time = -1;
         }
 
         System.out.println(time);
@@ -43,11 +51,12 @@ public class Main {
         // 행의개수 x < 열의개수 y : C연산 열에 대해서 수, 수 등장 횟수
         // 정렬 방식 count 오름차순, 동일할 시 넘버 오름차순
         List<List<Number>> list = new ArrayList<>();
+        int maxLen = Integer.MIN_VALUE;
 
         int[] num = new int[101];
         if ("R".equals(type)) {
             // List add
-            for (int i = 0; i < A[0].length; i++) {
+            for (int i = 0; i < A.length; i++) {
                 list.add(new ArrayList<Number>());
             }
 
@@ -55,7 +64,9 @@ public class Main {
             for (int i = 0; i < A.length; i++) {
                 Map<String, Integer> map = new HashMap<>();
                 for (int j = 0; j < A[i].length; j++) {
-                    map.put(String.valueOf(A[i][j]), map.getOrDefault(String.valueOf(A[i][j]), 0) + 1);
+                    if (A[i][j] > 0) {
+                        map.put(String.valueOf(A[i][j]), map.getOrDefault(String.valueOf(A[i][j]), 0) + 1);
+                    }
                 }
 
                 for (String key : map.keySet()) {
@@ -63,10 +74,61 @@ public class Main {
                 }
             }
 
+            // 정렬
+            for (int i = 0; i < list.size(); i++) {
+                Collections.sort(list.get(i));
+                maxLen = Math.max(maxLen, list.get(i).size());
+            }
 
+            // 배열 다시 세팅
+            int[][] copyA = new int[A.length][maxLen * 2];
+
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = 0; j < list.get(i).size(); j++) {
+                    copyA[i][j * 2] = list.get(i).get(j).number;
+                    copyA[i][(j * 2) + 1] = list.get(i).get(j).count;
+                }
+            }
+
+            A = copyA;
 
         } else {
 
+            for (int i = 0; i < A[0].length; i++) {
+                list.add(new ArrayList<Number>());
+            }
+
+            // 계산하기
+            for (int i = 0; i < A[0].length; i++) {
+                Map<String, Integer> map = new HashMap<>();
+                for (int j = 0; j < A.length; j++) {
+                    if (A[j][i] > 0) {
+                        map.put(String.valueOf(A[j][i]), map.getOrDefault(String.valueOf(A[j][i]), 0) + 1);
+                    }
+                }
+
+                for (String key : map.keySet()) {
+                    list.get(i).add(new Number(Integer.parseInt(key), map.get(key)));
+                }
+            }
+
+            // 정렬
+            for (int i = 0; i < list.size(); i++) {
+                Collections.sort(list.get(i));
+                maxLen = Math.max(maxLen, list.get(i).size());
+            }
+
+            // 배열 다시 세팅
+            int[][] copyA = new int[maxLen * 2][A[0].length];
+
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = 0; j < list.get(i).size(); j++) {
+                    copyA[j * 2][i] = list.get(i).get(j).number;
+                    copyA[(j * 2) + 1][i] = list.get(i).get(j).count;
+                }
+            }
+
+            A = copyA;
         }
     }
 
@@ -82,7 +144,7 @@ public class Main {
         @Override
         public int compareTo(Number o) {
             if (this.count == o.count) {
-                return o.number - this.number;
+                return this.number - o.number;
             }
             return this.count - o.count;
         }
@@ -96,5 +158,4 @@ public class Main {
         }
     }
 }
-
 

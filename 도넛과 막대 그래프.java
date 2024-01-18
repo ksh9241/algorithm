da1c4833f0a887e8ee43e0cbe77842c37cdec3bb
KimSeHwan <ksh9241@naver.com>
2024-01-18 23:01:21 +0900

    dfs 풀기

--- 도넛과 막대 그래프.java
+++ 도넛과 막대 그래프.java
@@ -1,9 +1,14 @@
-boolean[] visit = new boolean[1_000_001];
+import java.util.*;
+
+class Solution {
+    boolean[] visit = new boolean[1_000_001];
     List<List<Integer>> list = new ArrayList<>();
 
     int donutCount = 0; // idx 1
     int lineCount = 0; // idx 2
     int eightCount = 0; // idx 3
+
+    boolean check = false;
 
     public int[] solution(int[][] edges) {
         // Output 0번째 생성 정점, 1번째 도넛모양 개수(자기자신), 2번째 막대 그래프 개수, 3번째 8자모양 개수
@@ -12,114 +17,92 @@ boolean[] visit = new boolean[1_000_001];
         for (int i = 0; i < 1_000_001; i++) {
             list.add(new ArrayList<Integer>());
         }
+
+        // 생성노드 찾기
+        boolean[] addNodes = new boolean[1_000_001];
+        int maxIdx = 0;
+
+
         // 노드 세팅
         for (int i = 0 ; i < edges.length; i++) {
             int idx = edges[i][0];
             int value = edges[i][1];
 
+            addNodes[value] = true;
+            maxIdx = Math.max(maxIdx, idx);
             list.get(idx).add(value);
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).size() > 0) {
-                System.out.println(i + " == " + list.get(i));
+        // 생성노드 찾기
+        int addNode = 0;
+        for (int i = 1; i <= maxIdx; i++) {
+            if (!addNodes[i] && list.get(i).size() > 1) {
+                addNode = i;
+                visit[i] = true;
             }
         }
 
-
-        // 8 자 탐색
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).size() == 2 && !visit[i]) {
-                checkEight(i);
-            }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0 ; i < list.get(addNode).size(); i++) {
+            q.add(list.get(addNode).get(i));
         }
 
-        // 도넛 찾기
-        // 시작 노드가 종료노드와 같을 경우
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).size() == 1 && !visit[i]) {
-                checkDonut(i);
-            }
+        while(!q.isEmpty()) {
+            int idx = q.poll();
+            dfs(idx, idx, false);
+            check = false;
         }
+        donutCount += list.get(addNode).size();
 
-        System.out.println(donutCount + " / " + eightCount);
-
-        // 생성 노드 (8자노드, 도넛노드 빼고 남은 노드중 List가 가장 많은 노드)
+        answer = new int[]{addNode, donutCount, lineCount, eightCount};
+        // 생성 노드 (다른 노드에서 탐색하는 게 없을 때)
 
         return answer;
     }
+    public void dfs(int startIdx, int idx, boolean tooIdx) {
 
-    public void checkDonut(int startIdx) {
-        Set<Integer> checkSet = new HashSet<>();
-        boolean flag = false;
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(startIdx);
-
-        while (!q.isEmpty()) {
-            int idx = q.poll();
-            checkSet.add(idx);
-
-            if (startIdx == list.get(idx).get(0)) {
-                flag = true;
-                break;
-            }
-
-            if (list.get(idx).size() == 1) {
-                q.add(list.get(idx).get(0));
-            }
+        // 막대 그래프
+        if (list.get(idx).size() == 0 && !check) {
+            lineCount++;
+            donutCount--;
+            check = true;
+            return;
         }
 
-        if (flag) {
-            donutCount++;
-
-            for (int i : checkSet) {
-                visit[i] = true;
-            }
-        }
-    }
-
-    public void checkEight(int startIdx) {
-        Set<Integer> checkSet = new HashSet<>();
-        int upCount = 0;
-        int downCount = 0;
-        boolean flag = true;
-
-        for (int i = 0; i < list.get(startIdx).size(); i++) {
-
-            Queue<Integer> q = new LinkedList<>();
-            q.add(list.get(startIdx).get(i));
-
-            while(!q.isEmpty()) {
-                int idx = q.poll();
-                checkSet.add(idx);
-
-                // 한바퀴 탐색 후 시작노드로 돌아왔을 때 종료 조건
-                if (startIdx == idx) {
-                    break;
-                }
-
-                if (i % 2 == 0) {
-                    upCount++;
-                } else {
-                    downCount++;
-                }
-
-                // 연결고리 노드가 1개 이상이면 조건 불 충분
-                if (list.get(idx).size() != 1 || list.get(idx).get(0) == idx) {
+        // 8자 모양 그래프 || 도넛그래프
+        if (startIdx == idx && !check) {
+            boolean flag = true;
+            for (int i : list.get(idx)) {
+                if (!visit[i]) {
                     flag = false;
-                    break;
                 }
+            }
 
-                q.add(list.get(idx).get(0));
+            // 8자 그래프
+            if (flag && tooIdx) {
+                eightCount++;
+                donutCount--;
+                check = true;
+                return;
+            }
+            // 도넛그래프
+            if (flag && !tooIdx) {
+                check = true;
+                return;
             }
         }
 
-        if (flag && upCount == downCount) {
-            eightCount++;
-
-            for (int i : checkSet) {
-                visit[i] = true;
+        if (!check) {
+            if (list.get(idx).size() == 2) {
+                tooIdx = true;
+            }
+            for (int i = 0; i < list.get(idx).size(); i++) {
+                int nxtIdx = list.get(idx).get(i);
+                if (!visit[nxtIdx] || startIdx == nxtIdx) {
+                    visit[nxtIdx] = true;
+                    dfs(startIdx, nxtIdx, tooIdx);
+                }
             }
         }
     }
+}
